2263af2bb5aa31753b5faad3a70ef9c0bc5eb1d3
brianyu18 <brianyu1990@gmail.com>
2018-09-27 00:04:21 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,7 @@ def my_select(collection)
   i = 0
   result = []
   while i < collection.length
-    given_block?
-    if collection[i]
-    result << yield(collection[i])
+    if yield(collection[i])
     i += 1
   end
 result
